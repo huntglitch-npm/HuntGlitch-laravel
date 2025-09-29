7f69d995b2df71667dd1ed5103373749013b1fe7
Saurabh Gopani <saurabhg@itpathsolutions.com>
2025-09-29 16:44:52 +0530

    publish config

--- src/HuntglitchServiceProvider.php
+++ src/HuntglitchServiceProvider.php
@@ -12,12 +12,14 @@ class HuntglitchServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Load package routes
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        // Publish config file
+        $this->publishes([
+            __DIR__ . '/../config/huntglitch.php' => config_path('huntglitch.php'),
+        ], 'config');
 
         // Warn if config/huntglitch.php is missing
         if (!file_exists(config_path('huntglitch.php'))) {
-            Log::warning('[Huntglitch] Please run: php artisan vendor:publish --provider="Itpath\\Huntglitch\\HuntglitchServiceProvider" --tag=config to publish the Huntglitch config file.');
+            \Log::warning('[Huntglitch] Please run: php artisan vendor:publish --provider="Itpath\\Huntglitch\\HuntglitchServiceProvider" --tag=config to publish the Huntglitch config file.');
         }
     }
 
